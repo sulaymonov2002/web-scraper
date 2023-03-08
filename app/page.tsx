f3eaedc09d91b207d0b3e02de2a240227301aa3f
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-03-09 00:05:10 +0500

    feat: update new next.js app codes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { DocumentMagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 function HomePage() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <div>
+        <DocumentMagnifyingGlassIcon className="h-64 w-64 text-indigo-600/20" />
+
+        <h1 className="text-3xl mt-2 text-black font-bold mb-5">
+          Welcome to the Amazon Web Scraper
+        </h1>
+
+        <h2 className="text-lg italic text-center text-black/50">
+          To learn how to code from ZERO experience, join ZERO TO FULL STACK
+          HERO
+        </h2>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
