22cf9fc6ca4943065a76acd66de677fe70300909
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-03-13 00:07:50 +0500

    feat: add new functional codes

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,36 @@
+"use client";
+
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { FormEvent, useRef } from "react";
+
 function Header() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const input = inputRef.current?.value;
+    if (!input) return;
+
+    if (inputRef.current?.value) {
+      inputRef.current.value = "";
+    }
+  };
+
   return (
     <header>
-      <form>
-        <input type="text" />
-        <button>Search</button>
+      <form
+        className="flex items-center space-x-2 justify-center rounded-full py-2 px-4 bg-indigo-100 max-w-md mx-auto"
+        onSubmit={handleSearch}
+      >
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Search..."
+          className="flex-1 outline-none bg-transparent text-indigo-400 placeholder:text-indigo-300"
+        />
+        <button hidden>Search</button>
+        <MagnifyingGlassIcon className="h-6 w-6 text-indigo-300" />
       </form>
     </header>
   );
